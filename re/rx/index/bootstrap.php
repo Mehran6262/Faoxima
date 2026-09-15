5a77fb31d127a04aba70c5e7c2909c3762e4ba9c
Mehran6262 <167810297+Mehran6262@users.noreply.github.com>
2026-09-15 18:08:26 +0330

    Update bootstrap.php

--- re/rx/index/bootstrap.php
+++ re/rx/index/bootstrap.php
@@ -1,4 +1,28 @@
 <?php
+// === SELF-SUFFICIENT INPUT DERIVATION ===
+if (!isset($update) || !is_array($update)) {
+    $update = json_decode(file_get_contents('php://input'), true) ?: [];
+}
+
+if (empty($from_id)) {
+    $msg = $update['message'] ?? null;
+    $cb  = $update['callback_query'] ?? null;
+
+    $from_id    = $msg['from']['id'] ?? ($cb['from']['id'] ?? ($msg['chat']['id'] ?? 0));
+    $Chat_type  = $msg['chat']['type'] ?? ($cb['message']['chat']['type'] ?? null);
+    $text       = $msg['text'] ?? ($cb['data'] ?? '');
+    $chat_id    = $msg['chat']['id'] ?? ($cb['message']['chat']['id'] ?? 0);
+    $message    = $msg;
+    $first_name = $msg['from']['first_name'] ?? ($cb['from']['first_name'] ?? '');
+    $username   = $msg['from']['username'] ?? ($cb['from']['username'] ?? '');
+}
+
+rx_trace('AFTER_INPUT_DERIVE', [
+    'from_id'   => $from_id,
+    'Chat_type' => $Chat_type,
+    'text'      => mb_substr((string) ($text ?? ''), 0, 30),
+]);
+// === END INPUT DERIVATION ===
 
 // === START TRACE LOGGING ===
 if (!function_exists('rx_trace')) {
