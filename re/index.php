db016d8eaa9f338ec4ddf11fa05480eaba22acbf
Mehran6262 <167810297+Mehran6262@users.noreply.github.com>
2026-09-15 10:13:14 +0330

    Update index.php

--- re/index.php
+++ re/index.php
@@ -1,5 +1,7 @@
 <?php
 
+file_put_contents(__DIR__ . '/trace.log', "A: start\n", FILE_APPEND);
+
 ini_set('log_errors', '1');
 ini_set('error_log', __DIR__ . '/php_error.log');
 error_reporting(E_ALL);
@@ -15,10 +17,22 @@ file_put_contents(
     FILE_APPEND
 );
 
+file_put_contents(__DIR__ . '/trace.log', "B: before define root\n", FILE_APPEND);
+
 if (!defined('REFACTORED_LEGACY_ROOT')) {
     define('REFACTORED_LEGACY_ROOT', dirname(__DIR__));
 }
+
+file_put_contents(__DIR__ . '/trace.log', "C: after define root\n", FILE_APPEND);
+
 @chdir(REFACTORED_LEGACY_ROOT);
+
+file_put_contents(__DIR__ . '/trace.log', "D: after chdir\n", FILE_APPEND);
+
 require __DIR__ . '/_error_log.php';
+
+file_put_contents(__DIR__ . '/trace.log', "E: after _error_log.php\n", FILE_APPEND);
+
 require __DIR__ . '/rx/index/index.php';
 
+file_put_contents(__DIR__ . '/trace.log', "F: after rx/index/index.php\n", FILE_APPEND);
